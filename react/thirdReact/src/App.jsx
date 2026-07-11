be9095c99f3865f33a236aa30ad0c1d299812db5
krish101106 <krishgajjar051@gmail.com>
2026-07-11 17:19:24 +0530

    11June

---
+++ react/thirdReact/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1 className='bg-green-400 text-black rounded-xl p-4'>tailwind</h1>
+  )
+}
+
+export default App
